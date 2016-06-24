6c01f401bd340aae83d3134737284aa999170e86
Kazuhiro Oinuma <oinume@gmail.com>
2016-06-25 01:57:48 +0900

    Update dependencies

--- server/main.go
+++ server/main.go
@@ -1,19 +1,65 @@
 package main
 
 import (
-    "net/http"
-    "fmt"
-	"log"
+	"fmt"
+	"net/http"
+
+	"html/template"
+	"os"
+	"path"
+
+	"goji.io"
+	"goji.io/pat"
+	"golang.org/x/net/context"
 )
 
+var lekcijeEnv = os.Getenv("LEKCIJE_ENV")
+var templateDir = "static"
+
+func isProduction() bool {
+	return lekcijeEnv == "production"
+}
+
+func templatePath() string {
+	if isProduction() {
+		return "static"
+	} else {
+		return "src/www"
+	}
+}
+
+func init() {
+	if isProduction() {
+		templateDir = "static"
+	} else {
+		templateDir = "src/www"
+	}
+}
 func main() {
-    mux := http.NewServeMux()
-    mux.HandleFunc("/", root)
-	println("Listening on :5000")
-	log.Fatal(http.ListenAndServe(":5000", mux))
+	mux := mux()
+	fmt.Println("Listening on :5000")
+	http.ListenAndServe(":5000", mux)
 }
 
-func root(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+func mux() *goji.Mux {
+	mux := goji.NewMux()
+	mux.HandleFuncC(pat.Get("/"), index)
+	//mux.HandleFuncC(pat.Get("/:name"), index)
+	return mux
 }
 
+func index(ctx context.Context, w http.ResponseWriter, r *http.Request) {
+	t := template.Must(template.ParseFiles(path.Join(templateDir, "index.html")))
+	if err := t.Execute(w, nil); err != nil {
+		panic(err)
+	}
+}
+
+//func loadTemplate()
+//func index(ctx context.Context, w http.ResponseWriter, r *http.Request) {
+//	name := pat.Param(ctx, "name")
+//	if name == "" {
+//		name = r.URL.Query().Get("name")
+//	}
+//	fmt.Fprintf(w, "Hello, %s!", name)
+//}
