6f1198487628e631acacbbae45ec9ec0c7970f1a
Kazuhiro Oinuma <oinume@gmail.com>
2016-07-09 01:20:13 +0900

    Add Header component

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -2,34 +2,17 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Header from './components/header.jsx'
 
 let html = (
   <a href="/oauth/google" className="btn btn-primary">Google Sign in</a>
 );
 const apiToken = getCookie('apiToken');
+
 if (apiToken !== '') {
   html = (
     <div className="container">
-      <nav className="navbar navbar-inverse navbar-fixed-top">
-        <div className="container">
-          <div className="navbar-header">
-            <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-              <span className="sr-only">Toggle navigation</span>
-              <span className="icon-bar"></span>
-              <span className="icon-bar"></span>
-              <span className="icon-bar"></span>
-            </button>
-            <a className="navbar-brand" href="#">Project name</a>
-          </div>
-          <div id="navbar" className="collapse navbar-collapse">
-            <ul className="nav navbar-nav">
-              <li className="active"><a href="#">Home</a></li>
-              <li><a href="#about">About</a></li>
-              <li><a href="#contact">Contact</a></li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+      <Header />
       <hr />
       <div className="container">
         <div className="starter-template">
