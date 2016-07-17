e041d04f02b8bb962064c388dc6d7d6661575fc8
Kazuhiro Oinuma <oinume@gmail.com>
2016-07-17 14:29:07 +0900

    More markup teacher list

--- src/app/components/teacher.jsx
+++ src/app/components/teacher.jsx
@@ -3,11 +3,23 @@
 import React from 'react'
 
 export default class TeacherMain extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      teachers: [
+        { id: 1, name: "Xai" },
+        { id: 2, name: "Emina" },
+        { id: 3, name: "c" },
+      ]
+    };
+  }
+
   render() {
     return (
       <div className="container">
         <FollowTeacherForm />
-        <FollowingTeacherList />
+        <FollowingTeacherList teachers={this.state.teachers} />
       </div>
     );
   }
@@ -20,12 +32,15 @@ export class FollowTeacherForm extends React.Component {
         <h2>Follow a teacher!</h2>
         <form>
           <div className="form-group">
-            <label for="teacherUrlOrId">Teacher's URL or ID</label>
+            <label htmlFor="teacherUrlOrId">Teacher's URL or ID</label>
             <input type="text"
                    className="form-control"
                    id="teacherIdOrUrl"
                    name="teacherUrlOrId"
                    placeholder="URL or ID" />
+          </div>
+          <div className="form-group">
+            <button type="submit" className="btn btn-default">Submit</button>
           </div>
         </form>
       </div>
@@ -36,18 +51,29 @@ export class FollowTeacherForm extends React.Component {
 export class FollowingTeacherList extends React.Component {
 
   render() {
+    const teachers = [];
+    for (let teacher of this.props.teachers) {
+      teachers.push(<Teacher id={teacher.id} name={teacher.name} />);
+    }
     return (
       <div>
         <table className="table table-striped table-hover">
           <thead>
-            <tr><th>ID</th><th>Name</th></tr>
+          <tr><th>ID</th><th>Name</th></tr>
           </thead>
           <tbody>
-            <tr><td>1</td><td>Name1</td></tr>
-            <tr><td>2</td><td>Name2</td></tr>
+            {teachers}
           </tbody>
         </table>
       </div>
     );
+  }
+}
+
+export class Teacher extends React.Component {
+  render() {
+    return (
+      <tr><td>{this.props.id}</td><td>{this.props.name}</td></tr>
+    )
   }
 }
