02fec0acc3fc2df726dc26605382e5c31215699a
Kazuhiro Oinuma <oinume@gmail.com>
2016-07-29 05:31:30 +0900

    Change directory structure

--- webpack-production.config.js
+++ webpack-production.config.js
@@ -2,91 +2,89 @@ const webpack = require('webpack');
 const path = require('path');
 const buildPath = path.resolve(__dirname, 'static');
 const nodeModulesPath = path.resolve(__dirname, 'node_modules');
-const TransferWebpackPlugin = require('transfer-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const config = {
-    entry: [path.join(__dirname, '/src/app/main.js')],
-    resolve: {
-        //When require, do not have to add these extensions to file's name
-        extensions: ["", ".js"],
-        //node_modules: ["web_modules", "node_modules"]  (Default Settings)
-    },
-    //Render source-map file for final build
-    devtool: 'source-map',
-    //output config
-    output: {
-        path: buildPath,    //Path of output file
-        publicPath: "/static/",
-        filename: 'main.js',  //Name of output file
-    },
-    plugins: [
-        //Minify the bundle
-        new webpack.optimize.UglifyJsPlugin({
-            compress: {
-                //supresses warnings, usually from module minification
-                warnings: false,
-            },
-        }),
-        //Allows error warnings but does not stop compiling. Will remove when eslint is added
-        new webpack.NoErrorsPlugin(),
-        //Transfer Files
-        new TransferWebpackPlugin([
-            {from: 'www'},
-        ], path.resolve(__dirname,"src")),
+  entry: [path.join(__dirname, '/src/app/main.js')],
+  resolve: {
+    //When require, do not have to add these extensions to file's name
+    extensions: ["", ".js"],
+    //node_modules: ["web_modules", "node_modules"]  (Default Settings)
+  },
+  //Render source-map file for final build
+  devtool: 'source-map',
+  //output config
+  output: {
+    path: buildPath,
+    publicPath: "/static/",
+    filename: 'main.js',  //Name of output file
+  },
+  plugins: [
+    //Minify the bundle
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        //supresses warnings, usually from module minification
+        warnings: false,
+      },
+    }),
+    //Allows error warnings but does not stop compiling. Will remove when eslint is added
+    new webpack.NoErrorsPlugin(),
+    new CopyWebpackPlugin([
+      {
+        context: 'src',
+        from: '**/*.html',
+      },
+      {
+        context: 'src',
+        from: '**/*.css',
+      }
+    ]),
+  ],
+  module: {
+    loaders: [
+      {
+        //React-hot loader and
+        test: /\.jsx?$/,  //All .js files
+        loaders: ['react-hot', 'babel-loader'], //react-hot is like browser sync and babel loads jsx and es6-7
+        // query: {
+        //   presets: ['react', 'es2015']
+        // },
+        exclude: [nodeModulesPath]
+      },
+      {
+        test: /\.css$/,
+        loader: "style-loader!css-loader"
+      },
+      {
+        test: /\.png$/,
+        loader: "url-loader?limit=100000"
+      },
+      {
+        test: /\.jpg$/,
+        loader: "file-loader"
+      },
+      {
+        test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url?limit=10000&mimetype=application/font-woff'
+      },
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url?limit=10000&mimetype=application/octet-stream'
+      },
+      {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file'
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url?limit=10000&mimetype=image/svg+xml'
+      }
     ],
-    module: {
-        // loaders: [
-        //   {
-        //     test: /\.js$/, // All .js files
-        //     loaders: ['babel-loader'], //react-hot is like browser sync and babel loads jsx and es6-7
-        //     exclude: [nodeModulesPath],
-        //   },
-        // ],
-        loaders: [
-            {
-                //React-hot loader and
-                test: /\.jsx?$/,  //All .js files
-                loaders: ['react-hot', 'babel-loader'], //react-hot is like browser sync and babel loads jsx and es6-7
-                // query: {
-                //   presets: ['react', 'es2015']
-                // },
-                exclude: [nodeModulesPath]
-            },
-            {
-                test: /\.css$/,
-                loader: "style-loader!css-loader"
-            },
-            {
-                test: /\.png$/,
-                loader: "url-loader?limit=100000"
-            },
-            {
-                test: /\.jpg$/,
-                loader: "file-loader"
-            },
-            {
-                test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
-                loader: 'url?limit=10000&mimetype=application/font-woff'
-            },
-            {
-                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-                loader: 'url?limit=10000&mimetype=application/octet-stream'
-            },
-            {
-                test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-                loader: 'file'
-            },
-            {
-                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-                loader: 'url?limit=10000&mimetype=image/svg+xml'
-            }
-        ],
-    },
-    //Eslint config
-    eslint: {
-        configFile: '.eslintrc', //Rules for eslint
-    },
+  },
+  //Eslint config
+  eslint: {
+    configFile: '.eslintrc', //Rules for eslint
+  },
 };
 
 module.exports = config;
