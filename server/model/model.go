2d550881fe7586bc2d6ba84eb54d1cb4e058aa51
Kazuhiro Oinuma <oinume@gmail.com>
2016-08-21 00:21:13 +0900

    Set timezone as JST

--- server/model/model.go
+++ server/model/model.go
@@ -1,7 +1,6 @@
 package model
 
 import (
-	"fmt"
 	"os"
 	"time"
 
@@ -32,7 +31,7 @@ func Open() (*gorm.DB, error) {
 	dbDsn := os.Getenv("DB_DSN")
 	db, err := gorm.Open(
 		"mysql",
-		fmt.Sprintf("%v?charset=utf8mb4&parseTime=true&loc=Asia%2FTokyo&time_zone=JST", dbDsn),
+		dbDsn+"?charset=utf8mb4&parseTime=true&loc=Asia%2FTokyo",
 	)
 	if err != nil {
 		return nil, errors.InternalWrapf(err, "Failed to gorm.Open()")
