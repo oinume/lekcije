a3365aebb8ee0cb80e325a3c988d138b006f440d
Kazuhiro Oinuma <oinume@gmail.com>
2016-09-28 18:26:30 +0900

    Ignore not exising teacher in notifier

--- server/notifier/notifier.go
+++ server/notifier/notifier.go
@@ -55,7 +55,14 @@ func (n *Notifier) SendNotification(user *model.User) error {
 	for _, teacherID := range teacherIDs {
 		teacher, fetchedLessons, newAvailableLessons, err := n.fetchAndExtractNewAvailableLessons(teacherID)
 		if err != nil {
-			return err
+			switch err.(type) {
+			case *errors.NotFound:
+				// TODO: update teacher table flag
+				logger.AppLogger.Warn("Cannot fetch teacher", zap.Uint("teacherID", uint(teacherID)))
+				continue
+			default:
+				return err
+			}
 		}
 
 		allFetchedLessons = append(allFetchedLessons, fetchedLessons...)
@@ -82,7 +89,6 @@ func (n *Notifier) fetchAndExtractNewAvailableLessons(teacherID uint32) (
 ) {
 	teacher, fetchedLessons, err := lessonFetcher.Fetch(teacherID)
 	if err != nil {
-		// TODO: log?
 		logger.AppLogger.Error(
 			"TeacherLessonFetcher.Fetch",
 			zap.Uint("teacherID", uint(teacherID)), zap.Error(err),
