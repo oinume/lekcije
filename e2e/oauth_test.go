178e02c87c34a4d6b586b709ce8547ffba8dcd66
Kazuhiro Oinuma <oinume@gmail.com>
2016-10-15 06:49:12 +0900

    Skip the test on CircleCI

--- e2e/oauth_test.go
+++ e2e/oauth_test.go
@@ -15,48 +15,51 @@ var _ = time.UTC
 var _ = fmt.Print
 
 func TestOAuthGoogle(t *testing.T) {
+	if os.Getenv("CIRCLECI") != "" {
+		t.Skipf("Skip because it can't render Google log in page.")
+	}
 	a := assert.New(t)
 	driver := newWebDriver()
 	err := driver.Start()
-	a.NoError(err)
+	a.Nil(err)
 	defer driver.Stop()
 
 	page, err := driver.NewPage()
-	a.NoError(err)
-	a.NoError(page.Navigate(server.URL))
+	a.Nil(err)
+	a.Nil(page.Navigate(server.URL))
 	//time.Sleep(10 * time.Second)
 	link := page.FindByXPath("//div[@class='starter-template']/a")
 	u, err := link.Attribute("href")
-	a.NoError(err)
+	a.Nil(err)
 	fmt.Printf("u = %v, err = %v\n", u, err)
 	link.Click()
 	//time.Sleep(10 * time.Second)
 
 	googleAccount := os.Getenv("E2E_GOOGLE_ACCOUNT")
 	err = page.Find("#Email").Fill(googleAccount)
-	a.NoError(err)
+	a.Nil(err)
 	page.Find("#gaia_loginform").Submit()
-	a.NoError(err)
+	a.Nil(err)
 
 	time.Sleep(time.Second * 1)
 	page.Find("#Passwd").Fill(os.Getenv("E2E_GOOGLE_PASSWORD"))
-	a.NoError(err)
+	a.Nil(err)
 	page.Find("#gaia_loginform").Submit()
-	a.NoError(err)
+	a.Nil(err)
 
 	time.Sleep(time.Second * 3)
 	err = page.Find("#submit_approve_access").Click()
-	a.NoError(err)
+	a.Nil(err)
 	//time.Sleep(time.Second * 10)
 	// TODO: Check HTML content
 
 	cookies, err := page.GetCookies()
-	a.NoError(err)
+	a.Nil(err)
 	apiToken := getAPIToken(cookies)
 	a.NotEmpty(apiToken)
 
 	user, err := model.NewUserService(db).FindByUserAPIToken(apiToken)
-	a.NoError(err)
+	a.Nil(err)
 	a.Equal(googleAccount, user.Email.Raw())
 }
 
