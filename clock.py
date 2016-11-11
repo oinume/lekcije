13bf5ba0d40dcc22d16b03a486c58fa1a25cd0ae
Kazuhiro Oinuma <oinume@gmail.com>
2016-11-11 23:56:54 +0900

    3 -> 30

--- clock.py
+++ clock.py
@@ -2,8 +2,8 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 scheduler = BlockingScheduler()
 
-@scheduler.scheduled_job('interval', minutes=3)
+@scheduler.scheduled_job('interval', minutes=30)
 def timed_job():
-    print('This job is run every three minutes.')
+    print('This job is run every 30 minutes.')
 
 scheduler.start()
