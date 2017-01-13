ef04245fb883d06e3a174d3fba6197428a2a6f19
Kazuhiro Oinuma <oinume@gmail.com>
2017-01-13 22:37:48 +0900

    Add DB migration

---
+++ db/migrations/20161230110000_create_user_facebook.sql
@@ -0,0 +1,15 @@
+-- +goose Up
+-- SQL in section 'Up' is executed when this migration is applied
+CREATE TABLE IF NOT EXISTS user_facebook (
+  `facebook_id` varchar(255) NOT NULL,
+  `user_id` int unsigned NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`facebook_id`),
+  UNIQUE KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin ROW_FORMAT=DYNAMIC;
+
+-- +goose Down
+-- SQL section 'Down' is executed when this migration is rolled back
+DROP TABLE IF EXISTS user_facebook;
+
