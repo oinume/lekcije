53cc4a52b281e9990671baf53a3238c897b9057d
Kazuhiro Oinuma <oinume@gmail.com>
2017-02-18 00:49:43 +0900

    Small fix

--- bin/backup-mysql.sh
+++ bin/backup-mysql.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+set -e
+
 CWD=`pwd`
 DIR=`dirname $0`
 MYSQLDUMP="$CWD/$DIR/mysqldump"
-echo $MYSQLDUMP
+#echo $MYSQLDUMP
 
 TIMESTAMP=`date "+%Y%m%d_%H%M%S"`
-$MYSQLDUMP -u$MYSQL_USER -p$MYSQL_PASSWORD -h $MYSQL_HOST -P $MYSQL_PORT $MYSQL_DATABASE > $MYSQL_DATABASE_$TIMESTAMP.dump
+$MYSQLDUMP -u$MYSQL_USER -p$MYSQL_PASSWORD -h$MYSQL_HOST $MYSQL_DATABASE | bzip2 -9 > lekcije_$TIMESTAMP.dump.bz2
+aws s3 cp lekcije_$TIMESTAMP.dump.bz2 s3://lekcije.com/backup/
