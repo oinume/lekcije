8f9823817d70a48c3a3c262eac7174214d6ba784
Kazuhiro Oinuma <oinume@gmail.com>
2017-02-28 16:20:05 +0900

    Remove old MySQL backups

--- bin/backup-mysql.sh
+++ bin/backup-mysql.sh
@@ -7,6 +7,9 @@ DIR=`dirname $0`
 MYSQLDUMP="$CWD/$DIR/mysqldump"
 #echo $MYSQLDUMP
 
-TIMESTAMP=`date "+%Y%m%d_%H%M%S"`
-$MYSQLDUMP -u$MYSQL_USER -p$MYSQL_PASSWORD -h$MYSQL_HOST $MYSQL_DATABASE | bzip2 -9 > lekcije_$TIMESTAMP.dump.bz2
-aws s3 cp lekcije_$TIMESTAMP.dump.bz2 s3://lekcije.com/backup/
+DATE=`date "+%Y%m%d"`
+$MYSQLDUMP -u$MYSQL_USER -p$MYSQL_PASSWORD -h$MYSQL_HOST $MYSQL_DATABASE | bzip2 -9 > lekcije_$DATE.dump.bz2
+aws s3 cp lekcije_$DATE.dump.bz2 s3://lekcije.com/backup/
+
+DELETE_DATE=`date "+%Y%m%d" --date "7 days ago"`
+aws s3 ls s3://lekcije.com/backup/lekcije_$DELETE_DATE.dump.bz2 && aws s3 rm s3://lekcije.com/backup/lekcije_$DELETE_DATE.dump.bz2
