b8ce63a8f4298fade9d5ca0d445fdec634476a92
Kazuhiro Oinuma <oinume@gmail.com>
2017-03-03 23:02:43 +0900

    Add a test for emailer.Sender

--- server/emailer/sender.go
+++ server/emailer/sender.go
@@ -1,32 +1,48 @@
 package emailer
 
 import (
-	"os"
 	"fmt"
+	"os"
 	"strings"
+	"net/http"
 
-	"github.com/sendgrid/sendgrid-go/helpers/mail"
-	"github.com/sendgrid/sendgrid-go"
 	"github.com/oinume/lekcije/server/errors"
 	"github.com/oinume/lekcije/server/logger"
+	"github.com/sendgrid/rest"
+	"github.com/sendgrid/sendgrid-go"
+	"github.com/sendgrid/sendgrid-go/helpers/mail"
 )
 
 type Sender interface {
 	Send(email *Email) error
 }
 
-type SendGridSender struct{}
+type SendGridSender struct {
+	client *rest.Client
+}
 
-func NewSendGridSender() Sender {
-	return &SendGridSender{}
+func NewSendGridSender(httpClient *http.Client) Sender {
+	if httpClient == nil {
+		httpClient = http.DefaultClient
+	}
+	client := &rest.Client{
+		HTTPClient: httpClient,
+	}
+	return &SendGridSender{
+		client: client,
+	}
 }
 
 func (s *SendGridSender) Send(email *Email) error {
 	// TODO: Set HTTP Client
 	from := mail.NewEmail(email.From.Name, email.From.Address)
-	to := mail.NewEmail(email.Tos[0].Name, email.Tos[0].Address) // TODO
 	content := mail.NewContent("text/html", strings.Replace(email.BodyString(), "\n", "<br>", -1))
-	m := mail.NewV3MailInit(from, email.Subject, to, content)
+	tos := make([]*mail.Email, len(email.Tos))
+	for i, to := range email.Tos {
+		tos[i] = mail.NewEmail(to.Name, to.Address)
+	}
+	m := mail.NewV3MailInit(from, email.Subject, tos[0], content)
+	m.Personalizations[0].AddTos(tos[1:]...)
 
 	req := sendgrid.GetRequest(
 		os.Getenv("SENDGRID_API_KEY"),
@@ -35,7 +51,7 @@ func (s *SendGridSender) Send(email *Email) error {
 	)
 	req.Method = "POST"
 	req.Body = mail.GetRequestBody(m)
-	resp, err := sendgrid.API(req)
+	resp, err := s.client.API(req)
 	if err != nil {
 		return errors.InternalWrapf(err, "Failed to send email by sendgrid")
 	}
