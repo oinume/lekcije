a2da8bf0b598a860d1f9831788245db3e110cf2e
Kazuhiro Oinuma <oinume@gmail.com>
2017-03-07 14:38:27 +0900

    Add user.raw_email

---
+++ db/migrations/20170307050000_alter_user_add_raw_email.sql
@@ -0,0 +1,7 @@
+-- +goose Up
+-- SQL in section 'Up' is executed when this migration is applied
+ALTER TABLE user ADD COLUMN `raw_email` varchar(255) CHARACTER SET utf8mb4 NOT NULL AFTER email;
+
+-- +goose Down
+-- SQL section 'Down' is executed when this migration is rolled back
+ALTER TABLE user DROP COLUMN `raw_email`;
