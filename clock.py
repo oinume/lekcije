ad9f66414d6b397e7ab364f71c4ad714b800dcd4
Kazuhiro Oinuma <oinume@gmail.com>
2017-06-24 13:50:20 +0900

    Add future import

--- clock.py
+++ clock.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 from apscheduler.schedulers.blocking import BlockingScheduler
 import subprocess
 
@@ -5,11 +6,20 @@ scheduler = BlockingScheduler()
 
 
 @scheduler.scheduled_job('interval', minutes=1)
-def timed_job():
+def timed_job_min1():
     print("Run notifier")
     subprocess.run(
         "notifier -concurrency=5 -fetcher-cache=true -notification-interval=1 && curl -sS https://nosnch.in/c411a3a685",
         shell=True,
         check=True)
+
+
+# @scheduler.scheduled_job('interval', minutes=10)
+# def timed_job_min10():
+#     print("Run notifier")
+#     subprocess.run(
+#         "notifier -concurrency=5 -fetcher-cache=true -notification-interval=10 && curl -sS https://nosnch.in/c411a3a685",
+#         shell=True,
+#         check=True)
 
 scheduler.start()
