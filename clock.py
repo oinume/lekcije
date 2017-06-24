83f818c853d752e6e31d7857b44a899dab9267b7
Kazuhiro Oinuma <oinume@gmail.com>
2017-06-24 13:30:52 +0900

    Enable APScheduler

--- clock.py
+++ clock.py
@@ -3,9 +3,13 @@ import subprocess
 
 scheduler = BlockingScheduler()
 
-@scheduler.scheduled_job('interval', minutes=10)
+
+@scheduler.scheduled_job('interval', minutes=1)
 def timed_job():
     print("Run notifier")
-    subprocess.run("notifier && curl https://nosnch.in/c411a3a685", shell=True, check=True)
+    subprocess.run(
+        "notifier -concurrency=5 -fetcher-cache=true -notification-interval=1 && curl -sS https://nosnch.in/c411a3a685",
+        shell=True,
+        check=True)
 
 scheduler.start()
--- setup-python.sh
+++ setup-python.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ ! -d .venv ]; then
-    virtualenv --distribute --python /usr/local/bin/python3 .venv
+    virtualenv --distribute --python /usr/local/bin/python2 .venv
     . .venv/bin/activate
     pip install -r requirements.txt
 fi
