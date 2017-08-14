0cce928bd8f208fc9ba2859d28b4a89d4708aa9c
Kazuhiro Oinuma <oinume@gmail.com>
2017-08-14 23:18:25 +0900

    EmailField and SettingView component

--- frontend/js/setting.js
+++ frontend/js/setting.js
@@ -1,19 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// class EmailForm extends React.Component {
-//
-// //{{ template "_flashMessage.html" . }}
-//
-//   render() {
-//     return (
-//       <form method="POST" action="/me/setting/update">
-//         <div class="form-group">
-//           <label for="email">Email address</label>
-//           <input type="email" class="form-control" name="email" id="email" placeholder="Email" value="" required autofocus autocomplete="on" />
-//         </div>
-//         <button type="submit" class="btn btn-primary">送信</button>
-//       </form>
-//     );
-//   }
-// }
+class SettingView extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      email: 'oinume@gmail.com'
+    };
+  }
+
+  componentDidMount() {
+    // ここでGET /api/v1/@me/setting を呼んでデータを取得する
+  }
+
+  render() {
+    return (
+      <div>
+        <EmailField value={this.state.email} />
+      </div>
+    );
+  }
+}
+
+class EmailField extends React.Component {
+
+//{{ template "_flashMessage.html" . }}
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <form method="POST" action="/me/setting/update">
+        <div className="form-group">
+          <label htmlFor="email">Email address</label>
+          <input type="email" className="form-control" name="email" id="email" placeholder="Email"required autoFocus autoComplete="on" value={this.props.value} />
+        </div>
+        <button type="submit" className="btn btn-primary">送信</button>
+      </form>
+    );
+  }
+}
+
+ReactDOM.render(
+  <SettingView />,
+  document.getElementById('root')
+);
