e65ea725690ad9d655a1f855dc9733eeadf9056d
Kazuhiro Oinuma <oinume@gmail.com>
2018-01-04 22:34:04 +0900

    Fix typo

--- bin/migrate_daily_repoter.sh
+++ bin/migrate_daily_repoter.sh
@@ -6,7 +6,7 @@ END=2017-12-31
 CURRENT=$START
 while true; do
     echo $CURRENT
-    daily_repoter -target-date=$CURRENT
+    daily_reporter -target-date=$CURRENT
     if [ "$CURRENT" = "$END" ]; then
         break
     fi
