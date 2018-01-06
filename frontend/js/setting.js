0c9c2bce5a5ca5d4a948ddb81d8a5d180b7be7ba
Kazuhiro Oinuma <oinume@gmail.com>
2018-01-07 07:54:38 +0900

    Add NotificationTimeSpanFormContainer

--- frontend/js/setting.js
+++ frontend/js/setting.js
@@ -22,7 +22,7 @@ class SettingView extends MicroContainer {
     this.subscribe({
       fetch: this.handleFetch,
       onChange: this.handleOnChange,
-      update: this.handleUpdate, // TODO: rename to updateEmail
+      update: this.handleUpdate, // TODO: rename to updateEmail or Move to EmailFormContainer
       showAlert: this.handleShowAlert,
       hideAlert: this.handleHideAlert,
     });
@@ -36,12 +36,13 @@ class SettingView extends MicroContainer {
       <div>
         <Alert dispatch={this.dispatch} visible={a.visible} kind={a.kind} message={a.message}/>
         <EmailForm dispatch={this.dispatch} value={this.state.email}/>
-        <NotificationTimeSpanForm dispatch={this.dispatch}/>
+        <NotificationTimeSpanFormContainer rootDispatch={this.dispatch}/>
       </div>
     );
   }
 
   handleFetch() {
+    // TODO: ここでNotificationTimeSpanも取得
     const client = createClient();
     client.get('/api/v1/me/email')
       .then((response) => {
@@ -132,6 +133,28 @@ class EmailForm extends React.Component {
   }
 }
 
+class NotificationTimeSpanFormContainer extends MicroContainer {
+
+  constructor(props) {
+    super(props);
+    this.state = { editable: false };
+  }
+
+  componentDidMount() {
+    this.subscribe({
+      setEditable: this.handleSetEditable,
+    });
+  }
+
+  handleSetEditable(value) {
+    this.setState({ editable: value })
+  }
+
+  render() {
+    return <NotificationTimeSpanForm dispatch={this.dispatch} {...this.state}/>;
+  }
+}
+
 class NotificationTimeSpanForm extends React.Component {
   constructor(props) {
     super(props);
@@ -146,11 +169,18 @@ class NotificationTimeSpanForm extends React.Component {
             <p>12:40 〜 23:50</p>
             <p>12:40 〜 23:50</p>
             <p>12:40 〜 23:50</p>
+            {console.log(this.props.editable)}
           </div>
         </div>
         <div className="form-group">
           <div className="col-sm-offset-2 col-sm-8">
-            <button type="button" className="btn btn-default">編集</button>
+            <button
+              type="button"
+              className="btn btn-default"
+              onClick={() => this.props.dispatch('setEditable', true)}
+            >
+              編集
+            </button>
           </div>
         </div>
       </form>
