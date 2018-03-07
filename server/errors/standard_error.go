a7968d037d6fa1989e23bd6c5fafdc56a10799c8
Kazuhiro Oinuma <oinume@gmail.com>
2018-03-07 22:19:42 +0900

    Add StandardError

---
+++ server/errors/standard_error.go
@@ -0,0 +1,33 @@
+package errors
+
+import "github.com/pkg/errors"
+
+type Code int
+
+const (
+	CodeNotFound        Code = 1
+	CodeInvalidArgument Code = 2
+	CodeInternal        Code = 3
+)
+
+type StandardError struct {
+	code             Code
+	wrapped          error
+	cause            error
+	stackTrace       errors.StackTrace
+	outputStackTrace bool
+}
+
+func WrapStandardError(code Code, original error) *StandardError {
+	be := &StandardError{
+		code:    code,
+		wrapped: original,
+	}
+	if c, ok := be.wrapped.(Causer); ok {
+		be.cause = c.Cause()
+	}
+	if st, ok := be.wrapped.(StackTracer); ok {
+		be.stackTrace = st.StackTrace()
+	}
+	return be
+}
