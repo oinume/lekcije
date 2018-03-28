384e071e61d5cf0f95c315592a36661c14527c10
Kazuhiro Oinuma <oinume@gmail.com>
2018-03-28 10:45:56 +0900

    Should be NOT NULL

--- db/migrations/20180328050000_alter_teacher_add_last_lesson_at.sql
+++ db/migrations/20180328050000_alter_teacher_add_last_lesson_at.sql
@@ -1,6 +1,6 @@
 -- +goose Up
 -- SQL in section 'Up' is executed when this migration is applied
-ALTER TABLE teacher ADD COLUMN `last_lesson_at` DATETIME NULL AFTER `rating`;
+ALTER TABLE teacher ADD COLUMN `last_lesson_at` DATETIME NOT NULL AFTER `rating`;
 
 -- +goose Down
 -- SQL section 'Down' is executed when this migration is rolled back
