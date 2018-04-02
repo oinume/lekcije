7d894d4150577e51084dec2dbf81139d96a4c3df
Kazuhiro Oinuma <oinume@gmail.com>
2018-04-02 09:55:58 +0900

    Refactor tests

--- server/model/following_teacher_test.go
+++ server/model/following_teacher_test.go
@@ -36,19 +36,44 @@ func TestFollowingTeacherService_FindTeacherIDsByUserID(t *testing.T) {
 	a := assert.New(t)
 	r := require.New(t)
 
-	user := helper.CreateRandomUser()
-	teacher := helper.CreateRandomTeacher()
-	now := time.Now()
-	_, err := followingTeacherService.FollowTeacher(user.ID, teacher, now)
-	r.NoError(err)
+	t.Run("Can find teacherIDs", func(t *testing.T) {
+		user := helper.CreateRandomUser()
+		teacher := helper.CreateRandomTeacher()
+		now := time.Now()
+		_, err := followingTeacherService.FollowTeacher(user.ID, teacher, now)
+		r.NoError(err)
 
-	teacherIDs, err := followingTeacherService.FindTeacherIDsByUserID(user.ID, 5, defaultLastLessonAt)
-	r.NoError(err)
-	a.Equal(1, len(teacherIDs))
+		teacherIDs, err := followingTeacherService.FindTeacherIDsByUserID(user.ID, 5, defaultLastLessonAt)
+		r.NoError(err)
+		a.Equal(1, len(teacherIDs))
+	})
 
-	err = teacherService.IncrementFetchErrorCount(teacher.ID, 6)
-	r.NoError(err)
-	teacherIDs, err = followingTeacherService.FindTeacherIDsByUserID(user.ID, 5, defaultLastLessonAt)
-	r.NoError(err)
-	a.Equal(0, len(teacherIDs))
+	t.Run("Cannot find teacherIDs because of fetchErrorCount", func(t *testing.T) {
+		user := helper.CreateRandomUser()
+		teacher := helper.CreateRandomTeacher()
+		now := time.Now()
+		_, err := followingTeacherService.FollowTeacher(user.ID, teacher, now)
+		r.NoError(err)
+
+		err = teacherService.IncrementFetchErrorCount(teacher.ID, 6)
+		r.NoError(err)
+		teacherIDs, err := followingTeacherService.FindTeacherIDsByUserID(user.ID, 5, defaultLastLessonAt)
+		r.NoError(err)
+		a.Equal(0, len(teacherIDs))
+	})
+
+	t.Run("Cannot find teacherIDs because of lastLessonAt", func(t *testing.T) {
+		user := helper.CreateRandomUser()
+		teacher := helper.CreateRandomTeacher()
+		now := time.Now()
+		_, err := followingTeacherService.FollowTeacher(user.ID, teacher, now)
+		r.NoError(err)
+
+		teacher.LastLessonAt = time.Now().Add(-1 * 3 * 24 * time.Hour)
+		err = teacherService.CreateOrUpdate(teacher)
+		r.NoError(err)
+		teacherIDs, err := followingTeacherService.FindTeacherIDsByUserID(user.ID, 5, time.Now())
+		r.NoError(err)
+		a.Equal(0, len(teacherIDs))
+	})
 }
