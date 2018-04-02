9d01aef01af7a507640d73b94565e969715ec26b
Kazuhiro Oinuma <oinume@gmail.com>
2018-04-02 10:00:51 +0900

    Refactor tests

--- server/model/following_teacher_test.go
+++ server/model/following_teacher_test.go
@@ -26,9 +26,11 @@ func TestFollowingTeacherService_FollowTeacher(t *testing.T) {
 
 func TestFollowingTeacherService_CountFollowingTeachersByUserID(t *testing.T) {
 	a := assert.New(t)
+	r := require.New(t)
+
 	user := helper.CreateRandomUser()
 	count, err := followingTeacherService.CountFollowingTeachersByUserID(user.ID)
-	a.Nil(err)
+	r.Nil(err)
 	a.Equal(0, count)
 }
 
@@ -37,22 +39,17 @@ func TestFollowingTeacherService_FindTeacherIDsByUserID(t *testing.T) {
 	r := require.New(t)
 
 	t.Run("Can find teacherIDs", func(t *testing.T) {
-		user := helper.CreateRandomUser()
-		teacher := helper.CreateRandomTeacher()
-		now := time.Now()
-		_, err := followingTeacherService.FollowTeacher(user.ID, teacher, now)
+		user, teacher, err := followTeacher()
 		r.NoError(err)
 
 		teacherIDs, err := followingTeacherService.FindTeacherIDsByUserID(user.ID, 5, defaultLastLessonAt)
 		r.NoError(err)
 		a.Equal(1, len(teacherIDs))
+		a.Equal(teacher.ID, teacherIDs[0])
 	})
 
 	t.Run("Cannot find teacherIDs because of fetchErrorCount", func(t *testing.T) {
-		user := helper.CreateRandomUser()
-		teacher := helper.CreateRandomTeacher()
-		now := time.Now()
-		_, err := followingTeacherService.FollowTeacher(user.ID, teacher, now)
+		user, teacher, err := followTeacher()
 		r.NoError(err)
 
 		err = teacherService.IncrementFetchErrorCount(teacher.ID, 6)
@@ -63,10 +60,7 @@ func TestFollowingTeacherService_FindTeacherIDsByUserID(t *testing.T) {
 	})
 
 	t.Run("Cannot find teacherIDs because of lastLessonAt", func(t *testing.T) {
-		user := helper.CreateRandomUser()
-		teacher := helper.CreateRandomTeacher()
-		now := time.Now()
-		_, err := followingTeacherService.FollowTeacher(user.ID, teacher, now)
+		user, teacher, err := followTeacher()
 		r.NoError(err)
 
 		teacher.LastLessonAt = time.Now().Add(-1 * 3 * 24 * time.Hour)
@@ -76,4 +70,15 @@ func TestFollowingTeacherService_FindTeacherIDsByUserID(t *testing.T) {
 		r.NoError(err)
 		a.Equal(0, len(teacherIDs))
 	})
+}
+
+func followTeacher() (*User, *Teacher, error) {
+	user := helper.CreateRandomUser()
+	teacher := helper.CreateRandomTeacher()
+	now := time.Now()
+	_, err := followingTeacherService.FollowTeacher(user.ID, teacher, now)
+	if err != nil {
+		return nil, nil, err
+	}
+	return user, teacher, nil
 }
