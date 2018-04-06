8f321642ae1d12077fc3b635def846f662eb1d02
Kazuhiro Oinuma <oinume@gmail.com>
2018-04-06 13:45:36 +0900

    Add a test for NotificationTimeSpanList.Within

--- server/model/notification_time_span_test.go
+++ server/model/notification_time_span_test.go
@@ -47,10 +47,36 @@ func TestNotificationTimeSpan_Within(t *testing.T) {
 }
 
 func TestNotificationTimeSpanList_Within(t *testing.T) {
-	list := []*NotificationTimeSpanList{
-		&NotificationTimeSpan{
-			Number: 1,
+	table := []struct {
+		name         string
+		timeSpanList NotificationTimeSpanList
+		target       string
+		within       bool
+	}{
+		{
+			name: "",
+			timeSpanList: NotificationTimeSpanList{
+				&NotificationTimeSpan{
+					Number:   1,
+					FromTime: "01:00:00",
+					ToTime:   "02:00:00",
+				},
+				&NotificationTimeSpan{
+					Number:   2,
+					FromTime: "08:30:00",
+					ToTime:   "10:00:00",
+				},
+			},
+			target: "09:00:00",
+			within: true,
 		},
+	}
+
+	for _, tt := range table {
+		target, _ := time.Parse("15:04:05", tt.target)
+		if got := tt.timeSpanList.Within(target); got != tt.within {
+			t.Errorf("%v: unexpected value from Within: got=%v, want=%v", tt.name, got, tt.within)
+		}
 	}
 }
 
