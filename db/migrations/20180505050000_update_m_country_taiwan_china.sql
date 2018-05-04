52bfe8ff2ef624bfcb9ae58a221cdc2dc1bf3158
Kazuhiro Oinuma <oinume@gmail.com>
2018-05-05 02:38:58 +0900

    Update

--- db/migrations/20180505050000_update_m_country_taiwan_china.sql
+++ db/migrations/20180505050000_update_m_country_taiwan_china.sql
@@ -1,7 +1,7 @@
 -- +goose Up
 -- SQL in section 'Up' is executed when this migration is applied
 BEGIN;
-UPDATE `m_country` SET name_ja='台湾（台湾省/中華民国）' WHERE id = 158;
+UPDATE `m_country` SET name_ja='台湾' WHERE id = 158;
 UPDATE `m_country` SET name_ja='中華人民共和国' WHERE id = 156;
 COMMIT;
 
