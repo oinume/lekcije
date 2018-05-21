4286f47a5fe2433f75d5239e4f736c97b96cf971
Kazuhiro Oinuma <oinume@gmail.com>
2018-05-21 10:43:08 +0900

    Add test

---
+++ server/model/stat_daily_notification_event_test.go
@@ -0,0 +1,62 @@
+package model
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestStatDailyNotificationEventSeravice_CreateOrUpdate(t *testing.T) {
+	a := assert.New(t)
+	r := require.New(t)
+	helper.TruncateAllTables(helper.DB())
+
+	user1 := helper.CreateUser("test1", "test1@gmail.com")
+	user2 := helper.CreateUser("test2", "test2@gmail.com")
+	testCases := []struct {
+		userID   uint32
+		datetime time.Time
+		count    int
+	}{
+		{
+			userID:   user1.ID,
+			datetime: time.Date(2018, 1, 1, 1, 1, 1, 1, time.UTC),
+			count:    3,
+		},
+		{
+			userID:   user2.ID,
+			datetime: time.Date(2018, 1, 1, 1, 1, 1, 1, time.UTC),
+			count:    0,
+		},
+	}
+
+	for i, tc := range testCases {
+		err := createEventLogEmails(tc.userID, tc.datetime, tc.count)
+		r.NoError(err)
+		stats, err := eventLogEmailService.FindStatDailyNotificationEventByDate(tc.datetime)
+		r.NoError(err)
+		statUUs, err := eventLogEmailService.FindStatDailyNotificationEventUUCountByDate(tc.datetime)
+		r.NoError(err)
+
+		values := make(map[string]*StatDailyNotificationEvent, 100)
+		for _, s := range stats {
+			values[s.Event] = s
+		}
+		for _, s := range statUUs {
+			v := values[s.Event]
+			v.UUCount = s.UUCount
+			if err := statDailyNotificationEventService.CreateOrUpdate(v); err != nil {
+				r.NoError(err)
+			}
+		}
+
+		events, err := statDailyUserNotificationEventService.FindAllByDate(tc.datetime)
+		r.NoError(err)
+
+		r.Equal(len(testCases), len(events))
+		a.Equal(tc.userID, events[i].UserID)
+		a.Equal("open", events[i].Event)
+		a.EqualValues(tc.count, events[i].Count)
+	}
