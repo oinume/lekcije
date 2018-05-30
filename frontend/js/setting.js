da67c59fd9ac4f2befa87b3b0b14ed6ae5c22420
Kazuhiro Oinuma <oinume@gmail.com>
2018-05-30 21:17:53 +0900

    Add a test for EmailForm component

--- frontend/js/setting.js
+++ frontend/js/setting.js
@@ -4,8 +4,10 @@ import MicroContainer from 'react-micro-container';
 import {createHttpClient} from './http';
 import Alert from './components/Alert';
 import Select from './components/Select';
+import EmailForm from './components/setting/EmailForm';
 import {sprintf} from 'sprintf-js';
 import Loadable from 'react-loading-overlay';
+import PropTypes from "prop-types";
 
 class SettingView extends MicroContainer {
 
@@ -217,47 +219,52 @@ class SettingView extends MicroContainer {
   }
 }
 
-class EmailForm extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.onChange = this.onChange.bind(this);
-  }
-
-  onChange(e) {
-    this.props.dispatch('onChangeEmail', e.target.value);
-  }
-
-  render() {
-    return (
-      <form className="form-horizontal">
-        <div className="form-group">
-          <div className="col-sm-3">
-            <label htmlFor="email" className="control-label">Email</label>
-          </div>
-          <div className="col-sm-7">
-            <input
-              type="email" className="form-control" name="email" id="email"
-              placeholder="Email" required autoFocus autoComplete="on"
-              value={this.props.value} onChange={this.onChange}/>
-          </div>
-        </div>
-        <div className="form-group">
-          <div className="col-sm-offset-2 col-sm-8">
-            <button
-              type="button"
-              disabled={!this.props.value}
-              className="btn btn-primary"
-              onClick={() => this.props.dispatch('updateEmail', this.props.value)}
-            >
-              変更
-            </button>
-          </div>
-        </div>
-      </form>
-    );
-  }
-}
+// export class EmailForm extends React.Component {
+//
+//   constructor(props) {
+//     super(props);
+//     this.onChange = this.onChange.bind(this);
+//   }
+//
+//   onChange(e) {
+//     this.props.dispatch('onChangeEmail', e.target.value);
+//   }
+//
+//   render() {
+//     return (
+//       <form className="form-horizontal">
+//         <div className="form-group">
+//           <div className="col-sm-3">
+//             <label htmlFor="email" className="control-label">Email</label>
+//           </div>
+//           <div className="col-sm-7">
+//             <input
+//               type="email" className="form-control" name="email" id="email"
+//               placeholder="Email" required autoFocus autoComplete="on"
+//               value={this.props.value} onChange={this.onChange}/>
+//           </div>
+//         </div>
+//         <div className="form-group">
+//           <div className="col-sm-offset-2 col-sm-8">
+//             <button
+//               type="button"
+//               disabled={!this.props.value}
+//               className="btn btn-primary"
+//               onClick={() => this.props.dispatch('updateEmail', this.props.value)}
+//             >
+//               変更
+//             </button>
+//           </div>
+//         </div>
+//       </form>
+//     );
+//   }
+// }
+//
+// EmailForm.propTypes = {
+//   value: PropTypes.string.isRequired,
+//   dispatch: PropTypes.func.isRequired,
+// };
 
 class NotificationTimeSpanForm extends React.Component {
   constructor(props) {
