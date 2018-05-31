fd80c6878a115566d2906eadff4b30c37aa78ddb
Kazuhiro Oinuma <oinume@gmail.com>
2018-05-31 10:18:25 +0900

    Add test for NotificationTimeSpanForm

---
+++ frontend/test/NotificationTimeSpanForm.test.js
@@ -0,0 +1,30 @@
+'use strict';
+
+import React from 'react';
+import {configure, shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import MicroContainer from 'react-micro-container';
+import NotificationTimeSpanForm from "../js/components/setting/NotificationTimeSpanForm";
+
+configure({adapter: new Adapter()});
+
+test('<NotificationTimeSpanForm>', () => {
+  class Container extends MicroContainer {}
+  let container = new Container();
+  container.subscribe({
+    onChangeEmail: () => {},
+    updateEmail: () => {},
+  });
+ // container.dispatch('onChangeEmail', 'updateEmail');
+
+  let timeSpans = [];
+  const wrapper = shallow(
+    <NotificationTimeSpanForm
+      editable={false}
+      timeSpans={timeSpans}
+      dispatch={container.dispatch}
+    />
+  );
+  expect(wrapper.find('button')).toHaveLength(1);
+  // expect(wrapper.find('[name="email"]')).toHaveLength(1);
+});
