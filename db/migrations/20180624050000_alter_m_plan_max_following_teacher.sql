fdf143e6bbb0818c04fb14ade14bb954cb390b93
Kazuhiro Oinuma <oinume@gmail.com>
2018-07-09 21:56:35 +0900

    Update migration query

--- db/migrations/20180624050000_alter_m_plan_max_following_teacher.sql
+++ db/migrations/20180624050000_alter_m_plan_max_following_teacher.sql
@@ -1,12 +1,12 @@
 -- +goose Up
 -- SQL in section 'Up' is executed when this migration is applied
 ALTER TABLE m_plan
-  ADD COLUMN `max_following_teacher` TINYINT unsigned NOT NULL DEFAULT 0;
+  ADD COLUMN `max_following_teacher` TINYINT unsigned NOT NULL DEFAULT 0 AFTER `show_ad`;
 
 BEGIN;
-UPDATE m_plan SET max_following_teacher = 10 WHERE id = 1; /* Free */
-UPDATE m_plan SET max_following_teacher = 15 WHERE id = 2; /* Bronze */
-UPDATE m_plan SET max_following_teacher = 15 WHERE id = 3; /* Silver */
+UPDATE m_plan SET max_following_teacher = 10 WHERE id = 1 /* Free */;
+UPDATE m_plan SET max_following_teacher = 15 WHERE id = 2 /* Bronze */;
+UPDATE m_plan SET max_following_teacher = 15 WHERE id = 3 /* Silver */;
 COMMIT;
 
 -- +goose Down
