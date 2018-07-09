e344a1cdbeebf90d85ef6bf449b77cd486be4ac8
Kazuhiro Oinuma <oinume@gmail.com>
2018-07-09 21:32:16 +0900

    Add update query

--- db/migrations/20180624050000_alter_m_plan_max_following_teacher.sql
+++ db/migrations/20180624050000_alter_m_plan_max_following_teacher.sql
@@ -3,6 +3,12 @@
 ALTER TABLE m_plan
   ADD COLUMN `max_following_teacher` TINYINT unsigned NOT NULL DEFAULT 0;
 
+BEGIN;
+UPDATE m_plan SET max_following_teacher = 10 WHERE id = 1; /* Free */
+UPDATE m_plan SET max_following_teacher = 15 WHERE id = 2; /* Bronze */
+UPDATE m_plan SET max_following_teacher = 15 WHERE id = 3; /* Silver */
+COMMIT;
+
 -- +goose Down
 -- SQL section 'Down' is executed when this migration is rolled back
 ALTER TABLE m_plan DROP COLUMN `max_following_teacher`;
