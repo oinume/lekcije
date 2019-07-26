c2b3e199ab17c5b55ef63d8360586f6fe23d67c8
Kazuhiro Oinuma <oinume@gmail.com>
2019-07-26 13:49:54 +0900

    Remove dead code

--- server/notifier/main.go
+++ server/notifier/main.go
@@ -1,9 +1,7 @@
 package notifier
 
 import (
-	"bytes"
 	"context"
-	"encoding/binary"
 	"fmt"
 	"log"
 	"net/http"
@@ -139,30 +137,4 @@ func newStorageClient() (*storage.Client, error) {
 		os.Remove(f.Name())
 	}()
 	return storage.NewClient(context.Background(), option.WithCredentialsFile(f.Name()))
-}
-
-func doWork(ctx context.Context) {
-	// 4. Start a child span. This will be a child span because we've passed
-	// the parent span's ctx.
-	_, span := trace.StartSpan(ctx, "doWork")
-	// 5a. Make the span close at the end of this function.
-	defer span.End()
-
-	fmt.Println("doing busy work")
-	time.Sleep(80 * time.Millisecond)
-	buf := bytes.NewBuffer([]byte{0xFF, 0x00, 0x00, 0x00})
-	num, err := binary.ReadVarint(buf)
-	if err != nil {
-		// 6. Set status upon error
-		span.SetStatus(trace.Status{
-			Code:    trace.StatusCodeUnknown,
-			Message: err.Error(),
-		})
-	}
-
-	// 7. Annotate our span to capture metadata about our operation
-	span.Annotate([]trace.Attribute{
-		trace.Int64Attribute("bytes to int", num),
-	}, "Invoking doWork")
-	time.Sleep(20 * time.Millisecond)
 }
