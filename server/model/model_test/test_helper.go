d91ff408ec2b75c0e5268a7c32218bf84ae3314f
Kazuhiro Oinuma <oinume@gmail.com>
2019-08-13 15:50:15 +0900

    Add model_test package

---
+++ server/model/model_test/test_helper.go
@@ -0,0 +1,148 @@
+package model_test
+
+import (
+	"context"
+	"fmt"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/jinzhu/gorm"
+	"github.com/oinume/lekcije/server/config"
+	"github.com/oinume/lekcije/server/model"
+	"github.com/oinume/lekcije/server/util"
+)
+
+func ReplaceToTestDBURL(dbURL string) string {
+	if strings.HasSuffix(dbURL, "/lekcije") {
+		return strings.Replace(dbURL, "/lekcije", "/lekcije_test", 1)
+	}
+	return dbURL
+}
+
+type TestHelper struct {
+	dbURL string
+	db    *gorm.DB
+	//mCountryService *MCountryService
+}
+
+func NewTestHelper() *TestHelper {
+	return &TestHelper{}
+}
+
+func (h *TestHelper) DB(t *testing.T) *gorm.DB {
+	if h.db != nil {
+		return h.db
+	}
+	config.MustProcessDefault()
+	h.dbURL = ReplaceToTestDBURL(config.DefaultVars.DBURL())
+	db, err := model.OpenDB(h.dbURL, 1, config.DefaultVars.DebugSQL)
+	if err != nil {
+		panic(fmt.Sprintf("Failed to OpenDB(): err=%v", err))
+	}
+	h.db = db
+	return db
+}
+
+func (h *TestHelper) getDBName(dbURL string) string {
+	if index := strings.LastIndex(dbURL, "/"); index != -1 {
+		return dbURL[index+1:]
+	}
+	return ""
+}
+
+func (h *TestHelper) LoadAllTables(t *testing.T, db *gorm.DB) []string {
+	type Table struct {
+		Name string `gorm:"column:table_name"`
+	}
+	tables := []Table{}
+	sql := "SELECT table_name FROM information_schema.tables WHERE table_schema = ?"
+	if err := db.Raw(sql, h.getDBName(h.dbURL)).Scan(&tables).Error; err != nil {
+		panic(fmt.Sprintf("Failed to select table names: err=%v", err))
+	}
+
+	tableNames := []string{}
+	for _, t := range tables {
+		if t.Name == "goose_db_version" {
+			continue
+		}
+		tableNames = append(tableNames, t.Name)
+	}
+	return tableNames
+}
+
+func (h *TestHelper) TruncateAllTables(t *testing.T, db *gorm.DB) {
+	//fmt.Printf("TruncateAllTables() called!\n--- stack ---\n%+v\n", errors.NewInternalError().StackTrace())
+	tables := h.LoadAllTables(t, db)
+	for _, t := range tables {
+		if strings.HasPrefix(t, "m_") {
+			continue
+		}
+		if err := db.Exec("TRUNCATE TABLE " + t).Error; err != nil {
+			panic(fmt.Sprintf("Failed to truncate table: table=%v, err=%v", t, err))
+		}
+	}
+}
+
+func (h *TestHelper) CreateUser(t *testing.T, name, email string) *model.User {
+	db := h.DB(t)
+	user, err := model.NewUserService(db).Create(name, email)
+	if err != nil {
+		panic(fmt.Sprintf("Failed to CreateUser(): err=%v", err))
+	}
+	return user
+}
+
+func (h *TestHelper) CreateRandomUser(t *testing.T) *model.User {
+	name := util.RandomString(16)
+	return h.CreateUser(t, name, name+"@example.com")
+}
+
+func (h *TestHelper) CreateUserGoogle(t *testing.T, googleID string, userID uint32) *model.UserGoogle {
+	userGoogle := &model.UserGoogle{
+		GoogleID: googleID,
+		UserID:   userID,
+	}
+	if err := h.DB(t).Create(userGoogle).Error; err != nil {
+		panic(fmt.Sprintf("Failed to CreateUserGoogle(): %v", err))
+	}
+	return userGoogle
+}
+
+func (h *TestHelper) CreateTeacher(t *testing.T, id uint32, name string) *model.Teacher {
+	db := h.DB(t)
+	teacher := &model.Teacher{
+		ID:           id,
+		Name:         name,
+		Gender:       "female",
+		Birthday:     time.Date(2001, 1, 1, 0, 0, 0, 0, time.UTC),
+		LastLessonAt: time.Now().Add(-1 * 24 * time.Hour), // 1 day ago
+	}
+	if err := model.NewTeacherService(db).CreateOrUpdate(teacher); err != nil {
+		panic(fmt.Sprintf("Failed to CreateTeacher(): err=%v", err))
+	}
+	return teacher
+}
+
+func (h *TestHelper) CreateRandomTeacher(t *testing.T) *model.Teacher {
+	return h.CreateTeacher(t, uint32(util.RandomInt(9999999)), util.RandomString(6))
+}
+
+func (h *TestHelper) LoadMCountries(t *testing.T) *model.MCountries {
+	db := h.DB(t)
+	// TODO: cache
+	mCountries, err := model.NewMCountryService(db).LoadAll(context.Background())
+	if err != nil {
+		panic(fmt.Sprintf("Failed to MCountryService.LoadAll(): err=%v", err))
+	}
+	return mCountries
+}
+
+func (h *TestHelper) CreateFollowingTeacher(t *testing.T, userID uint32, teacher *model.Teacher) *model.FollowingTeacher {
+	now := time.Now()
+	ft, err := model.NewFollowingTeacherService(h.DB(t)).FollowTeacher(userID, teacher, now)
+	if err != nil {
+		panic(fmt.Sprintf("Failed to FollowTeacher(): err=%v", err))
+	}
+	return ft
+}
