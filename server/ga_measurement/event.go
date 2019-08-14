835b31df3c56c19ace01da5425a11a885deac33c
Kazuhiro Oinuma <oinume@gmail.com>
2019-08-14 11:19:45 +0900

    Add d ga_measurement

---
+++ server/ga_measurement/event.go
@@ -0,0 +1,11 @@
+package ga_measurement
+
+type EventValues struct {
+	UserAgentOverride string
+	ClientID          string
+	DocumentHostName  string
+	DocumentPath      string
+	DocumentTitle     string
+	DocumentReferrer  string
+	IPOverride        string
+}
