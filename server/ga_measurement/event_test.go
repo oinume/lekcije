6cd13cfe2ad5a510c283713776ba229ef749a9ce
Kazuhiro Oinuma <oinume@gmail.com>
2019-08-17 17:08:02 +0900

    Add unit test for ga_measurement.EventValues

---
+++ server/ga_measurement/event_test.go
@@ -0,0 +1,41 @@
+package ga_measurement
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"reflect"
+	"testing"
+)
+
+func TestNewEventValuesFromRequest(t *testing.T) {
+	const (
+		userAgent  = "go-test"
+		ipOverride = "1.1.1.1"
+	)
+	tests := map[string]struct {
+		request *http.Request
+		want    *EventValues
+	}{
+		"normal": {
+			request: httptest.NewRequest("POST", "http://localhost/event", nil),
+			want: &EventValues{
+				UserAgentOverride: userAgent,
+				ClientID:          "",
+				DocumentHostName:  "localhost",
+				DocumentPath:      "/event",
+				DocumentTitle:     "/event",
+				DocumentReferrer:  "",
+				IPOverride:        ipOverride,
+			},
+		},
+	}
+	for name, test := range tests {
+		test.request.Header.Set("User-Agent", userAgent)
+		test.request.Header.Set("X-Forwarded-For", ipOverride)
+		t.Run(name, func(t *testing.T) {
+			if got := NewEventValuesFromRequest(test.request); !reflect.DeepEqual(got, test.want) {
+				t.Errorf("NewEventValuesFromRequest() = %v, want %v", got, test.want)
+			}
+		})
+	}
+}
