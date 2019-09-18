505e0b093c95e7f3365962a69b48d74a8712b570
Kazuhiro Oinuma <oinume@gmail.com>
2019-09-18 21:16:56 +0900

    Fix "this authentication plugin is not supported"

--- db/docker-entrypoint-initdb.d/create_database.sql
+++ db/docker-entrypoint-initdb.d/create_database.sql
@@ -1,5 +1,4 @@
 CREATE USER IF NOT EXISTS 'lekcije'@'localhost' identified by 'lekcije';
-GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER, LOCK TABLES ON lekcije.* TO 'lekcije'@'localhost';
 CREATE USER IF NOT EXISTS 'lekcije'@'%' identified by 'lekcije';
 
 CREATE DATABASE IF NOT EXISTS lekcije DEFAULT CHARACTER SET utf8mb4 DEFAULT COLLATE utf8mb4_bin;
