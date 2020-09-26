df2f115eec48ea212d2f3cb2751fa3307c75c97b
Kazuhiro Oinuma <oinume@gmail.com>
2020-09-26 10:09:38 +0900

    Use liamstask/goose

--- tools/tools.go
+++ tools/tools.go
@@ -1,8 +1,8 @@
 package tools
 
 import (
-	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
-	_ "github.com/pressly/goose/cmd/goose"
+	_ "bitbucket.org/liamstask/goose/cmd/goose"
 	_ "github.com/golang/protobuf/protoc-gen-go"
+	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
 	_ "github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway"
 )
