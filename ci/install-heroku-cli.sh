c8fc7d97374c1e6f770d498c8d30e0f0f507c45a
Kazuhiro Oinuma <oinume@gmail.com>
2020-12-11 12:01:13 +0900

    Remove debug command

--- ci/install-heroku-cli.sh
+++ ci/install-heroku-cli.sh
@@ -1,22 +1,8 @@
 #!/bin/bash
 
-if [ -z "$HEROKU_LOGIN" -o -z "$HEROKU_API_KEY" ]; then
-    echo "Env HEROKU_LOGIN and HEROKU_API_KEY is required."
-    exit 1
-fi
-
 curl -L -o /tmp/heroku-linux-amd64.tar.gz https://cli-assets.heroku.com/branches/stable/heroku-linux-amd64.tar.gz
 tar -xz -C /opt -f /tmp/heroku-linux-amd64.tar.gz
-ln -s /opt/heroku/bin/heroku /usr/local/bin/heroku
-
-cat > ~/.netrc << EOF
-machine api.heroku.com
-login $HEROKU_LOGIN
-password $HEROKU_API_KEY
-machine git.heroku.com
-login $HEROKU_LOGIN
-password $HEROKU_API_KEY
-EOF
+#ln -s /opt/heroku/bin/heroku /usr/local/bin/heroku
 
 #git remote add heroku https://git.heroku.com/<your-app-name>.git
 #wget https://cli-assets.heroku.com/branches/stable/heroku-linux-amd64.tar.gz
