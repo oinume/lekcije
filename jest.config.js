1bda53cfa629cdba0b0c723ff330ef81f70610a7
Kazuhiro Oinuma <oinume@gmail.com>
2021-01-31 00:02:48 +0900

    Update config for jest

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,37 @@
+// module.exports = {
+//   verbose: true,
+//   rootDir: './frontend'
+// };
+
 module.exports = {
   verbose: true,
-  rootDir: './frontend'
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  rootDir: './frontend',
+  // roots: [
+  //   './frontend'
+  // ],
+//  setupFilesAfterEnv: ['<rootDir>/test/setupTests.ts'],
+  testPathIgnorePatterns: ['.next/', 'node_modules/'],
+//  snapshotSerializers: ['enzyme-to-json/serializer'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest'
+  },
+  moduleFileExtensions: [
+    'ts',
+    'tsx',
+    'js',
+    'jsx',
+    'json',
+    'node'
+  ],
+  // https://github.com/zeit/next.js/issues/8663#issue-490553899
+  globals: {
+    // we must specify a custom tsconfig for tests because we need the typescript transform
+    // to transform jsx into js rather than leaving it jsx such as the next build requires. you
+    // can see this setting in tsconfig.jest.json -> "jsx": "react"
+    'ts-jest': {
+      'tsconfig': 'tsconfig.jest.json'
+    }
+  }
 };
-
