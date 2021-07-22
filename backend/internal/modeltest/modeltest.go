509e23aacde95e386de773a87e13d4964c3c199a
Kazuhiro Oinuma <oinume@gmail.com>
2021-07-22 21:49:20 +0900

    Add randoms package

--- backend/internal/modeltest/modeltest.go
+++ backend/internal/modeltest/modeltest.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/oinume/lekcije/backend/model"
 	"github.com/oinume/lekcije/backend/model2"
-	"github.com/oinume/lekcije/backend/util"
+	"github.com/oinume/lekcije/randoms"
 )
 
 func NewNotificationTimeSpan(setters ...func(nts *model2.NotificationTimeSpan)) *model2.NotificationTimeSpan {
@@ -14,10 +14,10 @@ func NewNotificationTimeSpan(setters ...func(nts *model2.NotificationTimeSpan)) 
 		setter(timeSpan)
 	}
 	if timeSpan.UserID == 0 {
-		timeSpan.UserID = uint(util.RandomInt(10000000))
+		timeSpan.UserID = uint(randoms.MustNewInt64(10000000))
 	}
 	if timeSpan.Number == 0 {
-		timeSpan.Number = uint8(util.RandomInt(255))
+		timeSpan.Number = uint8(randoms.MustNewInt64(255))
 	}
 	if timeSpan.FromTime == "" {
 		timeSpan.FromTime = ""
@@ -34,10 +34,10 @@ func NewUser(setters ...func(u *model2.User)) *model2.User {
 		setter(user)
 	}
 	if user.Name == "" {
-		user.Name = "lekcije taro " + util.RandomString(8)
+		user.Name = "lekcije taro " + randoms.MustNewString(8)
 	}
 	if user.Email == "" {
-		user.Email = fmt.Sprintf("lekcije-%s@example.com", util.RandomString(8))
+		user.Email = fmt.Sprintf("lekcije-%s@example.com", randoms.MustNewString(8))
 	}
 	if user.PlanID == 0 {
 		user.PlanID = uint8(model.DefaultMPlanID)
@@ -51,10 +51,10 @@ func NewUserAPIToken(setters ...func(uat *model2.UserAPIToken)) *model2.UserAPIT
 		setter(userAPIToken)
 	}
 	if userAPIToken.Token == "" {
-		userAPIToken.Token = util.RandomString(32)
+		userAPIToken.Token = randoms.MustNewString(32)
 	}
 	if userAPIToken.UserID == 0 {
-		userAPIToken.UserID = uint(util.RandomInt(10000000))
+		userAPIToken.UserID = uint(randoms.MustNewInt64(10000000))
 	}
 	return userAPIToken
 }
@@ -65,7 +65,7 @@ func NewUserGoogle(setters ...func(ug *model2.UserGoogle)) *model2.UserGoogle {
 		setter(userGoogle)
 	}
 	if userGoogle.GoogleID == "" {
-		userGoogle.GoogleID = util.RandomString(32)
+		userGoogle.GoogleID = randoms.MustNewString(32)
 	}
 	return userGoogle
 }
