89de8bd16f1be4fa8211b9f0837fbf1b94ff015a
Kazuhiro Oinuma <oinume@gmail.com>
2021-08-03 23:32:28 +0900

    Remove dead code

--- frontend/js/components/setting/EmailForm.test.tsx
+++ frontend/js/components/setting/EmailForm.test.tsx
@@ -7,7 +7,6 @@ test('<EmailForm>', () => {
   const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {};
   const handleUpdateEmail = (email: string) => {};
   render(<EmailForm email="oinume@gmail.com" handleOnChange={handleOnChange} handleUpdateEmail={handleUpdateEmail} />);
-  // expect(wrapper.find('button')).toHaveLength(1);
-  // expect(wrapper.find('[name="email"]')).toHaveLength(1);
   expect(screen.getByDisplayValue('oinume@gmail.com')).toBeInTheDocument();
+  // TODO: write more assertions
 });
