3ea09c3a3b4c5606f8bb74b4b0ec39b7ea01eb71
Kazuhiro Oinuma <oinume@gmail.com>
2021-08-15 22:49:15 +0900

    Add usecase.ErrorReporter test

--- backend/usecase/error_recorder_test.go
+++ backend/usecase/error_recorder_test.go
@@ -1,6 +1,7 @@
-package rollbar_test
+package usecase_test
 
 import (
+	"bytes"
 	"context"
 	"testing"
 
@@ -9,14 +10,16 @@ import (
 	"github.com/oinume/lekcije/backend/errors"
 	"github.com/oinume/lekcije/backend/infrastructure/rollbar"
 	"github.com/oinume/lekcije/backend/internal/assertion"
+	"github.com/oinume/lekcije/backend/logger"
+	"github.com/oinume/lekcije/backend/usecase"
 )
 
-func Test_errorRecorderRepository_Record(t *testing.T) {
+func Test_ErrorRecorder_Record(t *testing.T) {
 	tests := map[string]struct {
-		client *rollbar.ClientMock
+		clientMock *rollbar.ClientMock
 	}{
-		"normal": {
-			client: &rollbar.ClientMock{
+		"ok": {
+			clientMock: &rollbar.ClientMock{
 				ErrorWithStackSkipWithExtrasAndContextFunc: func(ctx context.Context, level string, err error, skip int, extras map[string]interface{}) {
 					// nop
 				},
@@ -28,11 +31,14 @@ func Test_errorRecorderRepository_Record(t *testing.T) {
 	}
 	for name, tt := range tests {
 		t.Run(name, func(t *testing.T) {
-			r := rollbar.NewErrorRecorderRepository(tt.client)
-			ctx := context.Background()
+			var log bytes.Buffer
+			errorRecorder := usecase.NewErrorRecorder(
+				logger.NewAppLogger(&log, logger.NewLevel("info")),
+				rollbar.NewErrorRecorderRepository(tt.clientMock),
+			)
 			err := errors.NewAnnotatedError(errors.CodeInternal)
-			r.Record(ctx, err, "1")
-			assertion.AssertEqual(t, 1, len(tt.client.ErrorWithStackSkipWithExtrasAndContextCalls()), "")
+			errorRecorder.Record(context.Background(), err, "1")
+			assertion.AssertEqual(t, 1, len(tt.clientMock.ErrorWithStackSkipWithExtrasAndContextCalls()), "")
 		})
 	}
 }
