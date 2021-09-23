9b098bc6552bc4e9f91826ef33488fdeb92553d7
Kazuhiro Oinuma <oinume@gmail.com>
2021-09-23 17:23:21 +0900

    Add model2.MCountry

---
+++ backend/infrastructure/mysql/m_country.go
@@ -0,0 +1,23 @@
+package mysql
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/oinume/lekcije/backend/model2"
+	"github.com/oinume/lekcije/backend/repository"
+)
+
+type mCountryRepository struct {
+	db *sql.DB
+}
+
+func NewMCountryRepository(db *sql.DB) repository.MCountry {
+	return &mCountryRepository{
+		db: db,
+	}
+}
+
+func (r *mCountryRepository) FindAll(ctx context.Context) ([]*model2.MCountry, error) {
+	return model2.MCountries().All(ctx, r.db)
+}
