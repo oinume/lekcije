029da48dbc709c37af98c35504fcfb6eea6dca4d
Kazuhiro Oinuma <oinume@gmail.com>
2021-12-04 17:17:22 +0900

    Add API hook

---
+++ frontend/js/hooks/useListFollowingTeachers.ts
@@ -0,0 +1,25 @@
+import {useQuery} from 'react-query';
+import {NotificationTimeSpan} from '../models/NotificatonTimeSpan';
+import {TwirpError, twirpRequest} from '../http/twirp';
+import {Teacher} from '../models/Teacher';
+import {defaultUseQueryOptions, queryKeyFollowingTeachers} from './common';
+
+type ListFollowingTeachersRequest = Record<string, unknown>;
+
+type ListFollowingTeachersResponse = {
+  teachers: Teacher[]
+};
+
+export const useListFollowingTeachers = (
+  request: ListFollowingTeachersRequest,
+) => useQuery<ListFollowingTeachersResponse, TwirpError>(
+  queryKeyFollowingTeachers,
+  async () => {
+    const response = await twirpRequest('/twirp/api.v1.Me/ListFollowingTeachers', JSON.stringify(request));
+    const data = await response.json() as ListFollowingTeachersResponse;
+    console.log('----- data -----');
+    console.log(data);
+    return data;
+  },
+  defaultUseQueryOptions,
+);
