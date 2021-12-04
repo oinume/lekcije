d59e08b3a4edcf9d012e21f412bafd3c43bb4ea2
Kazuhiro Oinuma <oinume@gmail.com>
2021-12-04 17:18:04 +0900

    Define defaultUseQueryOptions

--- frontend/js/hooks/common.ts
+++ frontend/js/hooks/common.ts
@@ -1,1 +1,5 @@
 export const queryKeyMe = '/twirp/api.v1.Me/GetMe';
+export const queryKeyFollowingTeachers = '/twirp/api.v1.Me/ListFollowingTeachers';
+export const defaultUseQueryOptions = {
+  retry: 0
+};
