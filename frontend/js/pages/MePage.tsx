29a40fd5cf9bcb51cdbfd883aa730377e268ecd0
Kazuhiro Oinuma <oinume@gmail.com>
2021-12-04 13:38:01 +0900

    Add TeacherList component

--- frontend/js/pages/MePage.tsx
+++ frontend/js/pages/MePage.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {PageTitle} from '../components/PageTitle';
 import {useGetMe} from '../hooks/useGetMe';
 import {Loader} from '../components/Loader';
 import {ErrorAlert} from '../components/ErrorAlert';
+import {Teacher} from '../models/Teacher';
 
 export const MePage = () => {
   const getMeResult = useGetMe({});
@@ -30,22 +31,8 @@ type MeContentProps = {
 const MeContent = ({showTutorial}: MeContentProps) => (
   <>
     {showTutorial ? <Tutorial/> : <div/>}
-    <form method="POST" action="/me/followingTeachers/create">
-      <p>
-        講師のURLまたはIDを入力してフォローします<a href="https://lekcije.amebaownd.com/posts/2044879" rel="noreferrer" target="_blank"><i className="fas fa-question-circle button-help" aria-hidden="true"/></a><br/>
-        <small><a href="https://eikaiwa.dmm.com/" rel="noreferrer" target="_blank">DMM英会話で講師を検索</a></small>
-      </p>
-      <div className="input-group mb-3">
-        <input
-          id="teacherIdsOrUrl"
-          type="text"
-          className="form-control"
-          name="teacherIdsOrUrl"
-          placeholder="https://eikaiwa.dmm.com/teacher/index/492/"
-        />
-        <button type="submit" className="btn btn-primary">送信</button>
-      </div>
-    </form>
+    <CreateForm/>
+    <TeacherList/>
   </>
 );
 
@@ -60,4 +47,72 @@ const Tutorial = () => (
       <li>フォローすると、その講師の空きレッスンがあった時にメールでお知らせします</li>
     </ol>
   </div>
+);
+
+// https://getbootstrap.com/docs/4.4/components/spinners/
+const CreateForm = () => (
+  <form method="POST" action="/me/followingTeachers/create">
+    <p>
+      講師のURLまたはIDを入力してフォローします<a href="https://lekcije.amebaownd.com/posts/2044879" rel="noreferrer" target="_blank"><i className="fas fa-question-circle button-help" aria-hidden="true"/></a><br/>
+      <small><a href="https://eikaiwa.dmm.com/" rel="noreferrer" target="_blank">DMM英会話で講師を検索</a></small>
+    </p>
+    <div className="input-group mb-3">
+      <input
+        id="teacherIdsOrUrl"
+        type="text"
+        className="form-control"
+        name="teacherIdsOrUrl"
+        placeholder="https://eikaiwa.dmm.com/teacher/index/492/"
+      />
+      <button type="submit" className="btn btn-primary">送信</button>
+    </div>
+  </form>
+);
+
+const TeacherList = () => {
+  const teachers = [
+    new Teacher(28_660, 'hoge'),
+    new Teacher(28_661, 'fuga'),
+    new Teacher(28_662, 'aaaa'),
+  ];
+
+  const [checkedIds, setCheckedIds] = useState<number[]>([]);
+  const handleCheckboxOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const targetId = Number.parseInt(event.target.value, 10);
+    if (event.target.checked) {
+      setCheckedIds([...checkedIds, targetId]);
+    } else {
+      setCheckedIds(checkedIds.filter(id => id !== targetId));
+    }
+  };
+
+  return (
+    <div id="followingTeachers">
+      <form method="POST" action="/me/followingTeachers/delete">
+        <table className="table table-striped table-hover">
+          <thead>
+            <tr>
+              <th scope="col" className="col-md-1"><button type="submit" className="btn btn-primary btn-sm">削除</button></th>
+              <th scope="col" className="col-md-11">講師</th>
+            </tr>
+          </thead>
+          <tbody>
+            {teachers.map(t => <TeacherRow key={t.id} teacher={t} handleOnChange={handleCheckboxOnChange}/>)}
+          </tbody>
+        </table>
+      </form>
+    </div>
+  );
+};
+
+type TeacherRowProps = {
+  teacher: Teacher;
+  handleOnChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+const TeacherRow = ({teacher, handleOnChange}: TeacherRowProps) => (
+  <tr>
+    <td className="col-md-1"><input type="checkbox" name="teacherIds" value={teacher.id} onChange={handleOnChange}/></td>
+    <td className="col-md-8"><a href={`https://eikaiwa.dmm.com/teacher/index/${teacher.id}`} target="_blank" rel="noreferrer">{ teacher.name }</a></td>
+  </tr>
 );
