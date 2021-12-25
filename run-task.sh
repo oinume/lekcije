f94de8b7662fa8521d522ef2c2bab0cbed320949
Kazuhiro Oinuma <oinume@gmail.com>
2021-12-25 23:23:58 +0900

    Fix backup_mysql task is not running

--- run-task.sh
+++ run-task.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e
+set -ex
 
 echo $GCP_SERVICE_ACCOUNT_KEY | base64 --decode > ./gcp-service-account-key.json
 GOOGLE_APPLICATION_CREDENTIALS=./gcp-service-account-key.json invoke $1
