1f11a2a6e1455daf2dd96435d747d136f551ab52
Kazuhiro Oinuma <oinume@gmail.com>
2021-12-26 22:21:52 +0900

    Remove x flag

--- run-task.sh
+++ run-task.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -ex
+set -e
 
 echo $GCP_SERVICE_ACCOUNT_KEY | base64 --decode > ./gcp-service-account-key.json
 GOOGLE_APPLICATION_CREDENTIALS=./gcp-service-account-key.json invoke $1
