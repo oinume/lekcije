03c6f2c7dd0d6c5f8f78c64b07a1edaed28a1cd9
Kazuhiro Oinuma <oinume@gmail.com>
2022-03-26 19:46:30 +0900

    Add NOTE comment

--- backend/graph/model/user.go
+++ backend/graph/model/user.go
@@ -3,4 +3,5 @@ package model
 type User struct {
 	ID    string `json:"id"`
 	Email string `json:"email"`
+	// NOTE: FollowingTeachers is not defined intentionally because it is provided by userResolver
 }
