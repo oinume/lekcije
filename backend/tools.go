e9595100dcf80e8e7928bbf6a47c1ff582571141
Kazuhiro Oinuma <oinume@gmail.com>
2022-06-11 11:17:53 +0900

    Add build tag

--- backend/tools.go
+++ backend/tools.go
@@ -1,11 +1,14 @@
+//go:build tools
+// +build tools
+
 package tools
 
 import (
+	_ "github.com/99designs/gqlgen"
 	_ "github.com/golang/protobuf/protoc-gen-go"
 	_ "github.com/matryer/moq"
 	_ "github.com/pressly/goose/v3/cmd/goose"
 	_ "github.com/twitchtv/twirp/protoc-gen-twirp"
 	_ "github.com/volatiletech/sqlboiler/v4"
 	_ "github.com/volatiletech/sqlboiler/v4/drivers/sqlboiler-mysql"
-	_ "github.com/99designs/gqlgen"
 )
