dd98bb048ecbae8fb3d286c16ebf55dd4aed4dec
Kazuhiro Oinuma <oinume@gmail.com>
2022-09-23 16:05:08 +0900

    Fix goimports

--- backend/interface/graphql/resolver/notification_time_span.resolvers.go
+++ backend/interface/graphql/resolver/notification_time_span.resolvers.go
@@ -7,6 +7,7 @@ import (
 	"context"
 
 	"github.com/morikuni/failure"
+
 	lerrors "github.com/oinume/lekcije/backend/errors"
 	graphqlmodel "github.com/oinume/lekcije/backend/interface/graphql/model"
 )
