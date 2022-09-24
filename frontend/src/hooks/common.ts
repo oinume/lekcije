3e17a70135fefaf12d8652ffd6a84f6f82ce14bf
Kazuhiro Oinuma <oinume@gmail.com>
2022-09-24 11:31:25 +0900

    Remove twirp from frontend

--- frontend/src/hooks/common.ts
+++
@@ -1,8 +0,0 @@
-import type {UseQueryOptions} from '@tanstack/react-query';
-
-export const queryKeyMe = '/twirp/api.v1.Me/GetMe';
-export const queryKeyFollowingTeachers = '/twirp/api.v1.Me/ListFollowingTeachers';
-export const defaultUseQueryOptions: Omit<UseQueryOptions, any> = {
-  refetchOnMount: 'always',
-  retry: 0,
-};
