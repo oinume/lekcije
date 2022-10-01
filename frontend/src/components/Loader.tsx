4fe1c0be88471659690b6e57d61a4429c82949ad
Kazuhiro Oinuma <oinume@gmail.com>
2022-10-01 21:24:24 +0900

    Use CSSProperties

--- frontend/src/components/Loader.tsx
+++ frontend/src/components/Loader.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, {CSSProperties} from 'react';
 import {ClipLoader} from 'react-spinners';
 
 type Props = {
   isLoading: boolean;
   message?: string;
-  css?: string;
+  css?: CSSProperties;
   size?: number;
 };
 
@@ -15,7 +15,9 @@ export const Loader = ({isLoading, message, css, size}: Props) => {
   }
 
   if (css === undefined) {
-    css = 'background: rgba(255, 255, 255, 0)';
+    css = {
+      background: 'rgba(255, 255, 255, 0)',
+    };
   }
 
   if (size === undefined) {
@@ -25,7 +27,7 @@ export const Loader = ({isLoading, message, css, size}: Props) => {
   return isLoading ? (
     <div className="overlay-content">
       <div className="wrapper" style={{textAlign: 'center'}}>
-        <ClipLoader css={css} size={size} color="#123abc" loading={isLoading}/>
+        <ClipLoader cssOverride={css} size={size} color="#123abc" loading={isLoading}/>
         <p>
           <span className="message">{message}</span>
         </p>
