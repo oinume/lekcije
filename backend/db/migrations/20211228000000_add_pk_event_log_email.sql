56439aebe2f8b75476537684d40693e89f6574b2
Kazuhiro Oinuma <oinume@gmail.com>
2022-10-17 19:15:14 +0900

    Udpate event_log_email PK

--- backend/db/migrations/20211228000000_add_pk_event_log_email.sql
+++ backend/db/migrations/20211228000000_add_pk_event_log_email.sql
@@ -1,6 +1,8 @@
 -- +goose Up
 -- SQL in section 'Up' is executed when this migration is applied
-ALTER TABLE event_log_email ADD PRIMARY KEY (`datetime`, `event`, `email_type`);
+ALTER TABLE event_log_email
+    ADD COLUMN `id` int unsigned NOT NULL AUTO_INCREMENT FIRST,
+    ADD PRIMARY KEY (id);
 
 -- +goose Down
 -- SQL section 'Down' is executed when this migration is rolled back
