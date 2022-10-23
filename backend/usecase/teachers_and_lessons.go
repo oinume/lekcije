8ed7147ff56bdd918e6b28e4a7844093afb7d0b0
Kazuhiro Oinuma <oinume@gmail.com>
2022-10-23 22:17:19 +0900

    Move teachersAndLessons into another file

---
+++ backend/usecase/teachers_and_lessons.go
@@ -0,0 +1,65 @@
+package usecase
+
+import (
+	"bytes"
+	"fmt"
+	"time"
+
+	"github.com/oinume/lekcije/backend/model"
+	"github.com/oinume/lekcije/backend/model2"
+)
+
+type teachersAndLessons struct {
+	data         map[uint32]*model2.TeacherLessons
+	lessonsCount int
+	teacherIDs   []uint32
+}
+
+func (tal *teachersAndLessons) CountLessons() int {
+	count := 0
+	for _, l := range tal.data {
+		count += len(l.Lessons)
+	}
+	return count
+}
+
+// FilterBy filters out by NotificationTimeSpanList.
+// If a lesson is within NotificationTimeSpanList, it'll be included in returned value.
+func (tal *teachersAndLessons) FilterBy(list model.NotificationTimeSpanList) *teachersAndLessons {
+	if len(list) == 0 {
+		return tal
+	}
+	ret := newTeachersAndLessons(len(tal.data))
+	for teacherID, tl := range tal.data {
+		lessons := make([]*model2.Lesson, 0, len(tl.Lessons))
+		for _, lesson := range tl.Lessons {
+			dt := lesson.Datetime
+			t, _ := time.Parse("15:04", fmt.Sprintf("%02d:%02d", dt.Hour(), dt.Minute()))
+			if list.Within(t) {
+				lessons = append(lessons, lesson)
+			}
+		}
+		ret.data[teacherID] = model2.NewTeacherLessons(tl.Teacher, lessons)
+	}
+	return ret
+}
+
+func (tal *teachersAndLessons) String() string {
+	b := new(bytes.Buffer)
+	for _, tl := range tal.data {
+		_, _ = fmt.Fprintf(b, "Teacher: %+v", tl.Teacher)
+		_, _ = fmt.Fprint(b, ", Lessons:")
+		for _, l := range tl.Lessons {
+			_, _ = fmt.Fprintf(b, " {%+v}", l)
+		}
+	}
+	return b.String()
+}
+
+func newTeachersAndLessons(length int) *teachersAndLessons {
+	return &teachersAndLessons{
+		data:         make(map[uint32]*model2.TeacherLessons, length),
+		lessonsCount: -1,
+		teacherIDs:   make([]uint32, 0, length),
+	}
+}
---
+++ backend/usecase/teachers_and_lessons_test.go
@@ -0,0 +1,85 @@
+package usecase
+
+import (
+	"testing"
+	"time"
+
+	"github.com/oinume/lekcije/backend/internal/modeltest"
+	"github.com/oinume/lekcije/backend/model"
+	"github.com/oinume/lekcije/backend/model2"
+)
+
+func Test_TeachersAndLessons_FilterBy(t *testing.T) {
+	user := helper.CreateRandomUser(t)
+	timeSpans := []*model.NotificationTimeSpan{
+		{UserID: user.ID, Number: 1, FromTime: "15:30:00", ToTime: "16:30:00"},
+		{UserID: user.ID, Number: 2, FromTime: "20:00:00", ToTime: "22:00:00"},
+	}
+	//teacher := helper.CreateRandomTeacher(t)
+	teacher := modeltest.NewTeacher()
+	// TODO: table driven test
+	lessons := []*model2.Lesson{
+		{TeacherID: teacher.ID, Datetime: time.Date(2018, 1, 1, 15, 0, 0, 0, time.UTC)}, // excluded
+		{TeacherID: teacher.ID, Datetime: time.Date(2018, 1, 1, 16, 0, 0, 0, time.UTC)}, // included
+		{TeacherID: teacher.ID, Datetime: time.Date(2018, 1, 1, 17, 0, 0, 0, time.UTC)}, // excluded
+		{TeacherID: teacher.ID, Datetime: time.Date(2018, 1, 1, 21, 0, 0, 0, time.UTC)}, // included
+		{TeacherID: teacher.ID, Datetime: time.Date(2018, 1, 1, 23, 0, 0, 0, time.UTC)}, // excluded
+	}
+	tal := newTeachersAndLessons(10)
+	tal.data[uint32(teacher.ID)] = &model2.TeacherLessons{Teacher: teacher, Lessons: lessons}
+
+	filtered := tal.FilterBy(model.NotificationTimeSpanList(timeSpans))
+	if got, want := filtered.CountLessons(), 2; got != want {
+		t.Fatalf("unexpected filtered lessons count: got=%v, want=%v", got, want)
+	}
+
+	wantTimes := []struct {
+		hour, minute int
+	}{
+		{16, 0},
+		{21, 0},
+	}
+	tl := filtered.data[uint32(teacher.ID)]
+	for i, wantTime := range wantTimes {
+		if got, want := tl.Lessons[i].Datetime.Hour(), wantTime.hour; got != want {
+			t.Errorf("unexpected hour: got=%v, want=%v", got, want)
+		}
+		if got, want := tl.Lessons[i].Datetime.Minute(), wantTime.minute; got != want {
+			t.Errorf("unexpected minute: got=%v, want=%v", got, want)
+		}
+	}
+}
+
+func Test_TeachersAndLessons_FilterByEmpty(t *testing.T) {
+	//user := helper.CreateRandomUser()
+	timeSpans := make([]*model.NotificationTimeSpan, 0)
+	teacher := modeltest.NewTeacher()
+	// TODO: table driven test
+	lessons := []*model2.Lesson{
+		{TeacherID: teacher.ID, Datetime: time.Date(2018, 1, 1, 15, 0, 0, 0, time.UTC)},
+		{TeacherID: teacher.ID, Datetime: time.Date(2018, 1, 1, 16, 0, 0, 0, time.UTC)},
+	}
+	tal := newTeachersAndLessons(10)
+	tal.data[uint32(teacher.ID)] = &model2.TeacherLessons{Teacher: teacher, Lessons: lessons}
+
+	filtered := tal.FilterBy(model.NotificationTimeSpanList(timeSpans))
+	if got, want := filtered.CountLessons(), len(lessons); got != want {
+		t.Fatalf("unexpected filtered lessons count: got=%v, want=%v", got, want)
+	}
+
+	wantTimes := []struct {
+		hour, minute int
+	}{
+		{15, 0},
+		{16, 0},
+	}
+	tl := filtered.data[uint32(teacher.ID)]
+	for i, wantTime := range wantTimes {
+		if got, want := tl.Lessons[i].Datetime.Hour(), wantTime.hour; got != want {
+			t.Errorf("unexpected hour: got=%v, want=%v", got, want)
+		}
+		if got, want := tl.Lessons[i].Datetime.Minute(), wantTime.minute; got != want {
+			t.Errorf("unexpected minute: got=%v, want=%v", got, want)
+		}
+	}
+}
