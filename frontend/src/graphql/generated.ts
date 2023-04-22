16948a66387531b3edd247541c74f4fd03163025
Kazuhiro Oinuma <oinume@gmail.com>
2023-04-23 00:04:57 +0900

    update

--- frontend/src/graphql/generated.ts
+++ frontend/src/graphql/generated.ts
@@ -1,5 +1,5 @@
 import { GraphQLClient } from 'graphql-request';
-import { RequestInit } from 'graphql-request/src/types.dom';
+import { RequestInit } from 'graphql-request/dist/types.dom';
 import { useMutation, useQuery, UseMutationOptions, UseQueryOptions } from '@tanstack/react-query';
 export type Maybe<T> = T | null;
 export type InputMaybe<T> = Maybe<T>;
