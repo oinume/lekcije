53d7ce174ce455d70ab7973519579b64152d9f80
Kazuhiro Oinuma <oinume@gmail.com>
2023-07-08 17:38:07 +0900

    Add context func

--- backend/errors/failure.go
+++ backend/errors/failure.go
@@ -9,3 +9,19 @@ import (
 func NewUserIDContext(userID uint) failure.Context {
 	return failure.Context{"userID": fmt.Sprint(userID)}
 }
+
+func WithUserIDContext(c failure.Context, userID uint) failure.Context {
+	if c == nil {
+		c = failure.Context{}
+	}
+	c["userID"] = fmt.Sprint(userID)
+	return c
+}
+
+func WithTableContext(c failure.Context, table string) failure.Context {
+	if c == nil {
+		c = failure.Context{}
+	}
+	c["table"] = table
+	return c
+}
